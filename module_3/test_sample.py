59d77fb4f33836b8a2f505258429e48add1612c1
aleliv <alelivshits@gmail.com>
2021-02-18 15:47:17 +0300

    homework module_3 fixes

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -1,44 +1,38 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# -Пользовательский сценарий 1.2
-# Логин в личный кабинет
-# --Тестовый сценарий 1.2.1
-# Логин в личный кабинет
-# Предусловия:
-# Пользователь не залогинен
 
-try:
-    browser = webdriver.Chrome()
+def test_user_login():
+    # Data
+    login_page_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
 
-    # Открываем страницу логина
-    browser.get("http://selenium1py.pythonanywhere.com/ru/accounts/login")
+    title_text = 'Войти или зарегистрироваться | Oscar - Sandbox'
+    test_email = "testpochta@mail.ru"
+    test_passwd = "Q!W@E#R$T%"
+    try:
+        # Arrange
+        browser = webdriver.Chrome()
+        browser.get(login_page_link)
 
-    # Проверяем, что находимся на нужной транице
-    assert "Войти" in browser.title, "Открыта не страница авторизации"
+        wait_title = WebDriverWait(browser, 10)
+        wait_title.until(EC.title_is(title_text))
 
-    # Ввести зарегистрированный в системе адрес электронной почты
-    inputemail = browser.find_element_by_name("login-username")
-    inputemail.send_keys("testpochta@mail.ru")  # тестовый email
+        search_input_email = browser.find_element_by_name("login-username")
+        search_input_passwd = browser.find_element_by_name("login-password")
+        search_login_button = browser.find_element_by_name("login_submit")
 
-    # Ввести пароль в поле
+        # Act
+        search_input_email.send_keys(test_email)
+        search_input_passwd.send_keys(test_passwd)
+        search_login_button.click()
 
-    inputpswd = browser.find_element_by_name("login-password")
-    inputpswd.send_keys("Q!W@E#R$T%")  # тестовый пароль
+        # Assert
+        result_alert = browser.find_element_by_css_selector("div.alertinner").text
+        assert result_alert == "Рады видеть вас снова", "The user is not logged in"
 
-    # Нажать кнопку "Войти"
+    finally:
+        browser.quit()
 
-    button = browser.find_element_by_name("login_submit")
-    button.click()
 
-    # Проверяем успешный логин
-
-    alert = browser.find_element_by_css_selector("div.alertinner").text
-    assert alert == "Рады видеть вас снова", "Логин не произошел"
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    # time.sleep(12)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+test_user_login()
