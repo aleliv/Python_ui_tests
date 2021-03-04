6b177adc72cc81b0fedcb2c501fb2e3390b19f3b
aleliv <alelivshits@gmail.com>
2021-03-04 08:58:23 +0300

    homework 4 update

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -3,15 +3,18 @@ def test_add_to_cart_button_name(browser):
     page_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
     assertion_error_text = "The add to cart button's text is wrong"
-
+    add_to_cart_button_name = "Add to basket"
     add_to_cart_button_locator = ".btn-add-to-basket"
 
     # Arrange
     browser.get(page_link)
-    if browser.current_url.__contains__("ru"):
+
+    if browser.current_url.__contains__("com/ru"):
         add_to_cart_button_name = 'Добавить в корзину'
-    elif browser.current_url.__contains__("es"):
+    elif browser.current_url.__contains__("com/es"):
         add_to_cart_button_name = 'Añadir al carrito'
+    elif browser.current_url.__contains__("com/fr"):
+        add_to_cart_button_name = 'Ajouter au panier'
 
     # Act
     search_add_to_cart_button = browser.find_element_by_css_selector(add_to_cart_button_locator)
