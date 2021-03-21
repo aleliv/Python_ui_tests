d7fc1383ce6efec7a8067d69eabc8b8fc745cbee
aleliv <alelivshits@gmail.com>
2021-03-21 22:44:34 +0300

    homework 5, 5.5 lesson changes

--- module_5/pages/locators.py
+++ module_5/pages/locators.py
@@ -1,10 +1,19 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.XPATH, "//h1")
+    PRODUCT_MESSAGE_NAME = (By.CSS_SELECTOR, "div#messages > :nth-child(1) strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_MESSAGE = (By.CSS_SELECTOR, "div#messages > :nth-child(3) strong")
+    PRODUCT_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div#messages > :nth-child(1)")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
